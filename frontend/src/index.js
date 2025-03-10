016f1e4ccb02433d7fccb5445ab6fcbb20a2c692
EricLalumiere <eric.lalumiere@gmail.com>
2025-03-10 07:52:05 -0700

    clear js code for foundation reset

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-document.addEventListener("DOMContentLoaded", function() {
-    // Select the h1 element
-    const helloWorldElement = document.querySelector("h1");
-
-    // Change the text content
-    helloWorldElement.textContent = "Hello, JavaScript!";
-});
