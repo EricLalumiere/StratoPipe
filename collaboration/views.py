a0691d88f67a2ed3f40b6091d2a786d2e1279399
EricLalumiere <eric.lalumiere@gmail.com>
2025-03-10 14:47:41 -0700

    feat(collaboration/views.py): Add viewset for managing comments in collaborative workflows

---
+++ collaboration/views.py
@@ -0,0 +1,18 @@
+""" Views for the collaboration app. """
+
+from rest_framework import viewsets, permissions
+from .models import Comment
+from .serializers import CommentSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # For simplicity, allow users to see all comments (customize as needed)
+        return Comment.objects.all()
+
+    def perform_create(self, serializer):
+        # Set the comment's author as the current user
+        serializer.save(user=self.request.user)
