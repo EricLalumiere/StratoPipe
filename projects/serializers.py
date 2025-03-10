f20a3967630f87c51fb215e769370714c389ec9b
EricLalumiere <eric.lalumiere@gmail.com>
2025-03-10 08:11:10 -0700

    feat(projects/serializers.py): Add serializer for Project model

---
+++ projects/serializers.py
@@ -0,0 +1,17 @@
+""" Serializers for the projects app """
+
+from rest_framework import serializers
+from .models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+    """ Serializer for the Project model """
+    class Meta:
+        """ Meta class """
+        model = Project
+        fields = ['id', 'owner', 'name', 'description', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'owner', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        # Set the owner as the current authenticated user
+        validated_data['owner'] = self.context['request'].user
+        return super().create(validated_data)
