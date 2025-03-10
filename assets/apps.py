5022fa54fc68885119b15f6cbc6d02c5c5104323
EricLalumiere <eric.lalumiere@gmail.com>
2025-03-10 08:14:42 -0700

    feat(assets/apps.py): Define configuration for assets app

---
+++ assets/apps.py
@@ -0,0 +1,7 @@
+""" This module contains the configuration of the assets app. """
+
+from django.apps import AppConfig
+
+class AssetsConfig(AppConfig):
+    """ Configuration for the assets app. """
+    name = 'assets'
