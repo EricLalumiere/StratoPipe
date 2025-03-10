1e7874fcdccaa1b5e640309df57054ec5caf6b28
EricLalumiere <eric.lalumiere@gmail.com>
2025-03-10 07:26:46 -0700

    clears code for foundation reset

--- stratopipe/assets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AssetListCreateView, AssetDetailView
-
-urlpatterns = [
-    path('assets/', AssetListCreateView.as_view(), name='asset-list-create'),
-    path('assets/<int:pk>/', AssetDetailView.as_view(), name='asset-detail'),
-]
--- stratopipe/collaboration/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Comment
-from .serializers import CommentSerializer
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-timestamp')
-    serializer_class = CommentSerializer
