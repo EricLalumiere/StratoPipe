52d44abf3e310e67c70a6cbdd8703517ea3d30c3
EricLalumiere <eric.lalumiere@gmail.com>
2025-08-16 16:19:20 -0700

    Reintroduce owner field in Project model and update docstring for clarity

--- projects/models.py
+++ projects/models.py
@@ -1,14 +1,16 @@
 """ Project model """
 
 from django.db import models
-from django.contrib.auth.models import User
-# from django.conf import settings
+from django.conf import settings
 
 
 class Project(models.Model):
-    # owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='projects')
+    """
+      Represents a project in the system, containing assets and tasks.
+    """
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='projects')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
