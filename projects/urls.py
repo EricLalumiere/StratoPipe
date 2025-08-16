18aef4f65f98bd7c30900e34859afc188d331607
EricLalumiere <eric.lalumiere@gmail.com>
2025-08-16 16:34:44 -0700

    Refactor project URLs to use class-based views for cleaner and more maintainable routing

--- projects/urls.py
+++ projects/urls.py
@@ -1,12 +1,21 @@
 """ This module defines the URL patterns for the projects app. """
 
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from .views import ProjectViewSet
+#
+# router = DefaultRouter()
+# router.register(r'', ProjectViewSet, basename='project')
+#
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
 
-router = DefaultRouter()
-router.register(r'', ProjectViewSet, basename='project')
+# projects/urls.py
+from django.urls import path
+from .views import ProjectListCreateView, ProjectDetailView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', ProjectListCreateView.as_view(), name='project-list-create'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
 ]
